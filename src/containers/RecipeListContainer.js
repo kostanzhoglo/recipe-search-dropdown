4dbc236295b127dcaa11678a5c46f0a5161992ea
kostanzhoglo <jeffreywithers@gmail.com>
2018-10-11 09:12:54 -0400

    Add RecipeListContainer code

---
+++ src/containers/RecipeListContainer.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import RecipeSearch from '../components/RecipeSearch';
+import RecipeList from '../components/RecipeList';
+import ingredients from '../data/Ingredients'
+
+class RecipeListContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      recipes: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>I'm Hungry and in an Adventurous Mood!</h1>
+        <h2>Type some ingredients & search for a recipe...</h2>
+        <RecipeSearch
+          getRecipes={this.getRecipes}
+          suggestions={ingredients}
+        />
+        <RecipeList recipes={this.state.recipes} />
+      </div>
+    )
+  }
+
+  getRecipes = (userInput) => {
+    console.log(userInput)
+    // Two websites helped me with CORS issue when fetching w/o a backend:
+    // 1) https://github.com/Rob--W/cors-anywhere
+    // 2) https://stackoverflow.com/questions/29670703/how-to-use-cors-anywhere-to-reverse-proxy-and-add-cors-headers
+    fetch(`https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/?q=${userInput}`)
+      .then(res => res.json())
+      .then(response => {this.setState({ recipes: response.results})
+      }
+    )
+  }
+}
+
+export default RecipeListContainer
